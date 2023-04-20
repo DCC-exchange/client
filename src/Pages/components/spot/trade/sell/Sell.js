583e880585246b3ff45542efcd34613953ef9902
Highscore <highscoretechng@gmail.com>
2023-04-21 00:12:26 +0100

    we broke

--- src/Pages/components/spot/trade/sell/Sell.js
+++ src/Pages/components/spot/trade/sell/Sell.js
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function Sell() {
   return (
-    <div>Sell</div>
+    <div className="buy-limit">
+    <div className="buy-limit-container">
+        <div className="market-limit-btn">
+            <div className="market-limit-btn-container">
+                <div className="icon"></div>
+                <div className="btn">
+                    <button>Market</button>
+                </div>
+                <div className="icon"></div>
+            </div>
+            <div className="market-limit-btn-container">
+                <div className="icon"></div>
+                <div className="input">
+                    <input type="number" placeholder='Market Price'  />
+                </div>
+                <div className="icon"></div>
+            </div>
+              <div className="market-limit-btn-container">
+              <div className="icon"></div>
+                <div className="btn">
+                    <button>Market</button>
+                </div>
+                <div className="icon"></div>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
