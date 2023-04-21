6118912fdce21763992fac038d0929ffb2bedaa0
Highscore <highscoretechng@gmail.com>
2023-04-21 21:56:12 +0100

    updates

--- src/Pages/components/spot/route/Convert.js
+++ src/Pages/components/spot/route/Convert.js
@@ -1,7 +1,85 @@
 import React from 'react'
+import { FaAngleLeft } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom'
+import "../styles/convert.css"
 
 export default function Convert() {
+
+  const navigate = useNavigate()
+  const handleClose = (()=>{
+      navigate("/hot")
+  })
+
   return (
-    <div>Convert</div>
+    <div className="convert-page">
+      <div className="convert-page-container">
+          <div className="navigate-back"  onClick={handleClose}>
+            <h3><FaAngleLeft /></h3>
+          </div>
+          <div className="heading">
+              <h4>Convert</h4>
+          </div>
+          <div className="spend-coin">
+            <div className="spend-coin-container">
+                <div className="label">
+                  <div className="spend">
+                    <h4>Spend</h4>
+                  </div>
+                  <div className="available">
+                    <h4>Available 0 BTC</h4>
+                  </div>
+                </div>
+            </div>
+            <div className="spend-input">
+              <div className="spend-input-container">
+                  <div className="spend-coin-name">
+                      <img src="" alt="" />
+                      <h4>BTC</h4>
+                  </div>
+                  <div className="spend-coin-input">
+                      <input type="text" />
+                  </div>
+                  <div className="spend-coin-maximum">
+                      <p>Max</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <div className="spend-coin">
+            <div className="spend-coin-container">
+                <div className="label">
+                  <div className="spend">
+                    <h4>Receive</h4>
+                  </div>
+                </div>
+            </div>
+            <div className="spend-input">
+              <div className="spend-input-container">
+                  <div className="spend-coin-name">
+                      <img src="" alt="" />
+                      <h4>BTC</h4>
+                  </div>
+                  <div className="spend-coin-input">
+                      <input type="text" placeholder='Amount' />
+                  </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ref-price">
+              <div className="ref-price-container">
+                  <h4>Reference Price <span className='priec'>1BTC = 34.98 USDT</span> </h4>
+              </div>
+          </div>
+          <div className="convert-submit-btn">
+              <div className="convert-submit-btn-container">
+                  <button>Confirm</button>
+              </div>
+          </div>
+          <div className="convert-buttom-bal">
+              <h4>Balance</h4>
+          </div>
+      </div>
+    </div>
   )
 }
