20d60d796a373412c0b62d240d258c726a51270c
Highscore <highscoretechng@gmail.com>
2023-04-21 13:47:03 +0100

    updates

--- src/Pages/components/spot/trade/trade/BuyLimit.js
+++ src/Pages/components/spot/trade/trade/BuyLimit.js
@@ -3,36 +3,33 @@ import React, {useState} from 'react'
 export default function BuyLimit() {
 
     const [ marketPrice, setMarketPrice ] = useState(46.78)
+    const [ TradeAmount, setTradeAmount ] = useState("")
 
-    const AddPrice = ((e)=>{
-        if(e === "add"){
-            let newPrice = marketPrice + 0.1
-            setMarketPrice(newPrice)
-        }else{
-            let newPrice = marketPrice - 0.1
-            setMarketPrice(newPrice)
-        }
-    })
+
 
   return (
     <div className="market-limit">
         <div className="market-limit-btn-container">
-            <div className="icon" onClick={()=>AddPrice("add")}>
+            <div className="icon" onClick={()=>setMarketPrice(marketPrice + 0.1)}>
                 <h4>-</h4>
             </div>
             <div className="input">
                 <input type="number" value={marketPrice} onChange={(e)=> setMarketPrice(e.target.value)} placeholder='Market Price'  />
             </div>
-            <div className="icon" onClick={()=>AddPrice("asub")}>
+            <div className="icon" onClick={()=>setMarketPrice(marketPrice - 0.1)}>
                 <h4>+</h4>
             </div>
         </div>
         <div className="market-limit-btn-container">
-        <div className="icon"></div>
-            <div className="btn">
-                <button>Market</button>
+            <div className="icon" onClick={()=> setTradeAmount(TradeAmount + 0.1)}>
+                <h4>-</h4>
             </div>
-            <div className="icon"></div>
+            <div className="input">
+                <input type="number" value={TradeAmount} onChange={(e)=> setTradeAmount(e.target.value)} placeholder='Amount(ETH)'  />
+            </div>
+            <div className="icon" onClick={()=>setTradeAmount(TradeAmount - 0.1)}>
+                <h4>+</h4>
+            </div>
         </div>
         <div className="submit-btn-container">
             <div className="submit-btn">
