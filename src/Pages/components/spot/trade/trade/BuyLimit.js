202df36f360eba2f79ebb9d87452a2bdd50039ed
Highscore <highscoretechng@gmail.com>
2023-04-21 16:10:33 +0100

    updates

--- src/Pages/components/spot/trade/trade/BuyLimit.js
+++ src/Pages/components/spot/trade/trade/BuyLimit.js
@@ -5,6 +5,36 @@ export default function BuyLimit() {
     const [ marketPrice, setMarketPrice ] = useState(46.78)
     const [ TradeAmount, setTradeAmount ] = useState("")
 
+    const [ percentActive1, setPercentActive1 ] = useState("percentage-box-buy")
+    const [ percentActive2, setPercentActive2 ] = useState("percentage-boxEl")
+    const [ percentActive3, setPercentActive3 ] = useState("percentage-boxEl")
+    const [ percentActive4, setPercentActive4 ] = useState("percentage-boxEl")
+
+    const SetPercentageActive = ((e)=>{
+        if(e === 25){
+            setPercentActive1("percentage-box-buy")
+            setPercentActive2('percentage-boxEl')
+            setPercentActive3('percentage-boxEl')
+            setPercentActive4('percentage-boxEl')
+        }else if( e === 50){
+            setPercentActive1("percentage-box-buy")
+            setPercentActive2('percentage-box-buy')
+            setPercentActive3('percentage-boxEl')
+            setPercentActive4('percentage-boxEl')
+        }
+        else if( e === 75){
+            setPercentActive1("percentage-box-buy")
+            setPercentActive2('percentage-box-buy')
+            setPercentActive3('percentage-box-buy')
+            setPercentActive4('percentage-boxEl')
+        }else{
+            setPercentActive1("percentage-box-buy")
+            setPercentActive2('percentage-box-buy')
+            setPercentActive3('percentage-box-buy')
+            setPercentActive4('percentage-box-buy')
+        }
+    })
+
   return (
     <div className="market-limit">
         <div className="market-limit-btn-container">
@@ -31,33 +61,29 @@ export default function BuyLimit() {
         </div>
         <div className="amount-percentage">
             <div className="amount-percentage-container">
-                <div className="amount-percentage-contents">
-                    <div className="percentage-box">
-                        <div className="data"></div>
+                <div className="amount-percentage-contents" onClick={()=>SetPercentageActive(25)} >
+                    <div className={percentActive1}>
                     </div>
                     <div className="percentage-figure">
                         <h4>25%</h4>
                     </div>
                 </div>
-                <div className="amount-percentage-contents">
-                    <div className="percentage-box">
-                        <div className="data"></div>
+                <div className="amount-percentage-contents" onClick={()=>SetPercentageActive(50)}>
+                    <div className={percentActive2}>
                     </div>
                     <div className="percentage-figure">
                         <h4>50%</h4>
                     </div>
                 </div>
-                <div className="amount-percentage-contents">
-                    <div className="percentage-box">
-                        <div className="data"></div>
+                <div className="amount-percentage-contents" onClick={()=>SetPercentageActive(75)} >
+                    <div className={percentActive3}>
                     </div>
                     <div className="percentage-figure">
                         <h4>75%</h4>
                     </div>
                 </div>
-                <div className="amount-percentage-contents">
-                    <div className="percentage-box">
-                        <div className="data"></div>
+                <div className="amount-percentage-contents" onClick={()=>SetPercentageActive(100)}>
+                    <div className={percentActive4}>
                     </div>
                     <div className="percentage-figure">
                         <h4>100%</h4>
