ee5d0fbdb3ca968470a7b5341cebecbb84030f39
Highscore <highscoretechng@gmail.com>
2023-04-24 12:06:23 +0100

    god please

--- src/Pages/components/futures/coins/route/CoinM.js
+++ src/Pages/components/futures/coins/route/CoinM.js
@@ -1,19 +1,15 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { MdOutlineContentPasteSearch} from "react-icons/md";
 
 export default function CoinM() {
+
+  const [ data ] = useState(false)
+
   return (
     <div className='coin-drop-list'>
         <div className="coin-drop-list-container">
 
-            <div className="no-data">
-                <div className="no-data-container">
-                    <h4>No data</h4>
-                    <h4><MdOutlineContentPasteSearch /></h4>
-                </div>
-            </div>
-
-            <div className="coin-drop-list-content">
+            { data ?     <div className="coin-drop-list-content">
                 <div className="image-name-container">
                     <div className="image">
                       <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="" />
@@ -30,7 +26,14 @@ export default function CoinM() {
                         <h4>-3.57 </h4>
                     </div>
                 </div>
-            </div>
+            </div> :<div className="no-data">
+                <div className="no-data-container">
+                  <div className="no-data-content">
+                      <h4>No data</h4>
+                      <h3><MdOutlineContentPasteSearch /></h3>
+                  </div>
+                </div>
+            </div> }
 
         </div>
     </div>
