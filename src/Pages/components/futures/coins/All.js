27c24ba462665606a09f5fd10ee81c6c18b64b64
Highscore <highscoretechng@gmail.com>
2023-04-24 10:30:00 +0100

    god please

--- src/Pages/components/futures/coins/All.js
+++ src/Pages/components/futures/coins/All.js
@@ -32,7 +32,21 @@ export default function All() {
                     <h3>Usdt-M</h3>
                 </div>
             </div>
-            {  changeRoute ? <UsdtM /> : <CoinM /> }
+            <div className="search-coin-wrap">
+                <div className="search-coin-wrap-container">
+                    <div className="search-coin-wrap-header">
+                        <div className="search-coin-wrap-header-content">
+                            <div className="name">
+                                <h4>Name</h4>
+                            </div>
+                            <div className="price">
+                                <h4>Last Price/ 24H chg%</h4>
+                            </div>
+                        </div>
+                    </div>
+                      {  changeRoute ? <UsdtM /> : <CoinM /> }
+                </div>
+            </div>
         </div>
     </div>
   )
--- src/Pages/components/futures/coins/route/CoinM.js
+++ src/Pages/components/futures/coins/route/CoinM.js
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function CoinM() {
   return (
-    <div>CoinM</div>
+    <div className='coin-drop-list'>
+        <div className="coin-drop-list-container">
+            <div className="coin-drop-list-content">
+                <div className="image-name-container">
+                    <div className="image">
+                      <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="" />
+                    </div>
+                    <div className="pair">
+                        <h4>BTC <span>/USDT</span> </h4>
+                    </div>
+                </div>
+                <div className="lastprice-container">
+                    <div className="price">
+                        <h4>27,9087</h4>
+                    </div>
+                    <div className="percent">
+                        <h4>BTC <span>/USDT</span> </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
