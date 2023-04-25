abd1ce1fcd79b903a094f5c5d1248f125514a93f
Highscore <highscoretechng@gmail.com>
2023-04-25 14:54:21 +0100

    god please

--- src/Pages/components/futures/Futures.js
+++ src/Pages/components/futures/Futures.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import "./style/futures.css"
 import "./style/all.css"
+import "./style/position.css"
 
 export default function Futures() {
   return (
