ae5508ca0909a43367a789b1b076f0b5276867dc
Highscore <highscoretechng@gmail.com>
2023-04-25 11:08:48 +0100

    updates

--- src/Pages/components/futures/trade/trade/BuyLimit.js
+++ src/Pages/components/futures/trade/trade/BuyLimit.js
@@ -8,29 +8,41 @@ export default function BuyLimit() {
 
     const [TotalAmount, setTotalAmount] = useState('')
 
-    const [ percentActive1, setPercentActive1 ] = useState("percentage-boxEl")
-    const [ percentActive2, setPercentActive2 ] = useState("percentage-boxEl")
-    const [ percentActive3, setPercentActive3 ] = useState("percentage-boxEl")
-    const [ percentActive4, setPercentActive4 ] = useState("percentage-boxEl")
+    const [ percentActive, setPercentActive ] = useState("future-percent-content")
+    const [ percentActive1, setPercentActive1 ] = useState("future-percent-content")
+    const [ percentActive2, setPercentActive2 ] = useState("future-percent-content")
+    const [ percentActive3, setPercentActive3 ] = useState("future-percent-content")
+    const [ percentActive4, setPercentActive4 ] = useState("future-percent-content")
 
     const SetPercentageActive = ((e)=>{
-        if(e === 25){
+        if(e === 0){
+            setPercentActive("percentage-box-buy")
+            setPercentActive1('percentage-boxEl')
+            setPercentActive2('percentage-boxEl')
+            setPercentActive3('percentage-boxEl')
+            setPercentActive4('percentage-boxEl')
+        }
+        else if(e === 25){
+            setPercentActive("percentage-box-buy")
             setPercentActive1("percentage-box-buy")
             setPercentActive2('percentage-boxEl')
             setPercentActive3('percentage-boxEl')
             setPercentActive4('percentage-boxEl')
         }else if( e === 50){
+            setPercentActive("percentage-box-buy")
             setPercentActive1("percentage-box-buy")
             setPercentActive2('percentage-box-buy')
             setPercentActive3('percentage-boxEl')
             setPercentActive4('percentage-boxEl')
         }
         else if( e === 75){
+            setPercentActive("percentage-box-buy")
             setPercentActive1("percentage-box-buy")
             setPercentActive2('percentage-box-buy')
             setPercentActive3('percentage-box-buy')
             setPercentActive4('percentage-boxEl')
         }else{
+            setPercentActive("percentage-box-buy")
             setPercentActive1("percentage-box-buy")
             setPercentActive2('percentage-box-buy')
             setPercentActive3('percentage-box-buy')
@@ -65,22 +77,22 @@ export default function BuyLimit() {
 
         <div className="futures-percent">
             <div className="futures-percent-container">
-                <div onClick={()=>SetPercentageActive(0)} className="future-percent-content">
+                <div onClick={()=>SetPercentageActive(0)} className={percentActive}>
                     <h3><TbSquareRotated /></h3>
                 </div>
-                <div onClick={()=>SetPercentageActive(25)} className="future-percent-content">
+                <div onClick={()=>SetPercentageActive(25)} className={percentActive1}>
                     <div className="connection"></div>
                     <h3><TbSquareRotated /></h3>
                 </div>
-                <div onClick={()=>SetPercentageActive(50)} className="future-percent-content">
+                <div onClick={()=>SetPercentageActive(50)} className={percentActive2}>
                     <div className="connection"></div>
                     <h3><TbSquareRotated /></h3>
                 </div>
-                <div onClick={()=>SetPercentageActive(75)} className="future-percent-content">
+                <div onClick={()=>SetPercentageActive(75)} className={percentActive3}>
                     <div className="connection"></div>
                     <h3><TbSquareRotated /></h3>
                 </div>
-                <div onClick={()=>SetPercentageActive(100)} className="future-percent-content">
+                <div onClick={()=>SetPercentageActive(100)} className={percentActive4}>
                     <div className="connection"></div>
                     <h3><TbSquareRotated /></h3>
                 </div>
