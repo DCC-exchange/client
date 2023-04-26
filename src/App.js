e614a22c6289c2e5282e6228632b110ca3e12f85
Highscore <highscoretechng@gmail.com>
2023-04-26 17:35:29 +0100

    dreams

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import React, {useEffect} from "react"
 import { useNavigate } from "react-router-dom";
 import Welcome from "./Pages/components/welcome/Index";
@@ -33,21 +33,15 @@ import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
 
-  const Navigate  = useNavigate()
-
   // Welcome first timer
   useEffect(()=>{
   const first = JSON.parse(localStorage.getItem("firstHit"));
     if(!first){
-      Navigate ('/welcome')
+      <Navigate to="/welcome" />
     }
-  },[Navigate ])
+  },[])
 
   const { user } = useAuthContext()
-
-  const handleProtect = (()=>{
-    Navigate ('/sign/login')
-  })
 
   return (
     <div className="App">
@@ -97,7 +91,7 @@ function App() {
             <Route path="/marketChart" element={<MarketChart />} />
             
             {/* Wallet routes */}
-            <Route path="wallet" element={user ? <Assets /> : handleProtect }>
+            <Route path="wallet" element={user ? <Assets /> : <Navigate to="/sign" /> }>
                 <Route index element={<Overview />} />
                 <Route path="over-view" element={<Overview />} />
                 <Route path="spot" element={<SpotWallet />} />
