7e25652b5863b86eed16dcb48a625ccb109a313d
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-04-29 18:30:17 +0100

    create chart

--- src/Pages/components/market/components/Candlestick.js
+++ src/Pages/components/market/components/Candlestick.js
@@ -1,31 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useLocation } from 'react-router-dom';
 import axios from "axios";
+import { createChart } from 'lightweight-charts';
 
 const Candlestick = () => {
   const location = useLocation();
   const { id } = location.state;
+  const myRef = useRef(null);
 
-  const fetchOhlcData = async() => {
-    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/ohlc?days=1&vs_currency=usd`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        vs_currency: 'usd',
-      })
-    })
-    console.log(response)
+  const fetchOhlcData = async (newDiv) => {
+    const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/ohlc?days=1&vs_currency=usd`);
+    const data = response.data;
+    const cData = data.map((d) => {
+      return { time: d[0], open: d[1], high: d[2], low: d[3], close: d[4] }
+    });
+    const chartOptions = { height: 400, layout: { textColor: 'white', background: { type: 'solid', color: 'transparent' } } };
+    const chart = createChart(newDiv, chartOptions);
+    const candlestickSeries = chart.addCandlestickSeries({ upColor: '#26a69a', downColor: '#ef5350', borderVisible: false, wickUpColor: '#26a69a', wickDownColor: '#ef5350' });
+    chart.timeScale().fitContent();
+    candlestickSeries.setData(cData);
+    console.log(cData[0]);
   }
 
   useEffect(() => {
-    fetchOhlcData();
+    const newNode = myRef.current;
+    const newDiv = newNode.appendChild(document.createElement('div'));
+    console.log(newDiv);
+    
+    fetchOhlcData(newDiv);
   }, [id])
 
   return (
-    <div>
-      <p>Candlestick</p>
+    <div ref={myRef}>
     </div>
   )
 }
