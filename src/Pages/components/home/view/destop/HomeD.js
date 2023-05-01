e5a22b32810de3fc12c9ab2fa3251d0a0b21a6e1
Highscore <highscoretechng@gmail.com>
2023-05-01 19:40:21 +0100

    home desktop page

--- src/Pages/components/home/view/destop/HomeD.js
+++ src/Pages/components/home/view/destop/HomeD.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import homeImg from "./image/homeImg.png"
 
 export default function HomeD() {
   return (
@@ -7,8 +8,35 @@ export default function HomeD() {
         <div className="design-circle"></div>
         <Navbar />
         <div className="home-page-container">
-
-            <h2>Destop Home</h2>
+          <div className="home-text-container">
+              <div className="home-text-content">
+                  <div className="heading-text">
+                      <div className="heading-text-container">
+                          <h1><span>D</span>DCC <span>Y</span>our <span>Ex</span>change</h1>
+                      </div>
+                  </div>
+                  <div className="detailed-text">
+                      <div className="detailed-text-container">
+                          <h4>Buy And Sell BTC, ETH, LTC, DOGE and other altcoin</h4>
+                      </div>
+                  </div>
+                  <div className="home-email-search">
+                      <div className="home-email-search-container">
+                          <div className="email-container">
+                              <input type="text" placeholder='Email' />
+                          </div>
+                          <div className="button-container">
+                              <button>Get started</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="home-img-content">
+                  <div className="home-img-container">
+                      <img src={homeImg} alt="" />
+                  </div>
+              </div>
+          </div>
         </div>
     </div>
   )
