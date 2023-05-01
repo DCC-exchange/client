385677bb162cd5bafe11916c98725ee187162cce
Highscore <highscoretechng@gmail.com>
2023-05-01 17:09:41 +0100

    Main page

--- src/Pages/components/home/view/destop/HomeD.js
+++ src/Pages/components/home/view/destop/HomeD.js
@@ -4,8 +4,10 @@ import Navbar from './component/Navbar'
 export default function HomeD() {
   return (
     <div className="home-page">
+        <div className="design-circle"></div>
         <Navbar />
         <div className="home-page-container">
+
             <h2>Destop Home</h2>
         </div>
     </div>
