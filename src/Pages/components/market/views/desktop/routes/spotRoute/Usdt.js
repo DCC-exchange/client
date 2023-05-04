c560547fa7d9b8bca9f11440c6fe841f6248528f
Highscore <highscoretechng@gmail.com>
2023-05-04 14:33:19 +0100

    market route

--- src/Pages/components/market/views/desktop/routes/spotRoute/Usdt.js
+++ src/Pages/components/market/views/desktop/routes/spotRoute/Usdt.js
@@ -13,10 +13,40 @@ export default function Usdt() {
       }
     })
 
-  return (
-    <div>
-         <h3 onClick={handleStarred}> { starred ? <AiFillStar /> : <AiOutlineStar /> } </h3>
 
+  return (
+    <div className='desktop-market-coins'>
+        <div className="desktop-market-coins-container">
+            <div className="logo-pairs">
+                <div className="star">
+                    <h3 onClick={handleStarred}> { starred ? <AiFillStar /> : <AiOutlineStar /> } </h3>
+                </div>
+                <div className="logo">
+                    <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="" />
+                </div>
+                <div className="pair">
+                    <h4>BTC<span>/USDT</span></h4>
+                </div>
+            </div>
+            <div className="price">
+                <h4>23,456.00</h4>
+            </div>
+            <div className="change-percent">
+                <h4>2.34%</h4>
+            </div>
+            <div className="high">
+                <h4>23,456.00</h4>
+            </div>
+            <div className="high">
+                <h4>23,456.00</h4>
+            </div>
+            <div className="high">
+                <h4>23,456.00</h4>
+            </div>
+            <div className="trade">
+                <h4>Trade</h4>
+            </div>
+        </div>
     </div>
   )
 }
