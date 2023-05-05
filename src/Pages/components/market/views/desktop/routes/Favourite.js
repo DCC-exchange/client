2ac0dc30d0c361e4ce755e4b6e32e5e3f5c39b9f
Highscore <highscoretechng@gmail.com>
2023-05-05 22:46:44 +0100

    home desktop page

--- src/Pages/components/market/views/desktop/routes/Favourite.js
+++ src/Pages/components/market/views/desktop/routes/Favourite.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Spot from './favouriteRoute/Spot'
+import Coin from './favouriteRoute/Coin'
+import Usdt from './favouriteRoute/Usdt'
+
 
 export default function Favourite() {
+
+  const [ spotActive, setSpotactive ] = useState(true)
+  const [ usdtActive, setUsdtactive ] = useState(false)
+  const [ coinActive, setCoinActive ] = useState(false)
+
+  const [ defaultRoute, setDefault  ] = useState(<Usdt />)
+
+  const handleNavigation = ((e)=>{
+      if(e === 1){
+          setDefault(<Spot />)
+          setSpotactive(true)
+          setUsdtactive(false)
+          setCoinActive(false)
+      }else if(e === 2) {
+          setDefault(<Usdt />)
+          setSpotactive(false)
+          setUsdtactive(true)
+          setCoinActive(false)
+      }else{
+        setDefault(<Coin />)
+        setSpotactive(false)
+        setUsdtactive(false)
+        setCoinActive(true)
+      }
+  })
+
   return (
-    <div>Favourite</div>
+    <div className='destop-market-spot-route'>
+    <div className="market-spot-route-contain">
+        <div className="navigation-route">
+            <div className="navigation-route-container">
+                <div className="navigation-route-contents">
+                    <button onClick={()=>handleNavigation(1)} className={ spotActive ? "active" : 'normal' } >Spot</button>
+                </div>
+                <div className="navigation-route-contents">
+                    <button onClick={()=>handleNavigation(2)} className={ usdtActive ? "active" : 'normal' } >USDT-M</button>
+                </div>
+                <div className="navigation-route-contents">
+                    <button onClick={()=>handleNavigation(3)} className={ coinActive ? "active" : 'normal' } >COIN-M</button>
+                </div>
+            </div>
+        </div>
+
+        <div className="market-dropdown-heading">
+            <div className="market-dropdown-heading-container">
+                <div className="market-dropdown-heading-content">
+                    <h4>Pair</h4>
+                </div>
+                <div className="market-dropdown-heading-content">
+                    <h4>Price</h4>
+                </div>
+                <div className="market-dropdown-heading-content">
+                    <h4>24H change%</h4>
+                </div>
+                <div className="market-dropdown-heading-content">
+                    <h4>24H High</h4>
+                </div>
+                <div className="market-dropdown-heading-content">
+                    <h4>24H Low</h4>
+                </div>
+                <div className="market-dropdown-heading-content">
+                    <h4>24H  Volume (USDT)</h4>
+                </div>
+                <div className="market-dropdown-heading-content">
+                    <h4>Trade</h4>
+                </div>
+            </div>
+        </div>
+        { defaultRoute }
+    </div>
+</div>
   )
 }
