7750211103324d1664f0662c99df1d5163823ba2
Highscore <highscoretechng@gmail.com>
2023-05-05 17:24:10 +0100

    market route

--- src/Pages/components/home/view/destop/routes/Spot.js
+++ src/Pages/components/home/view/destop/routes/Spot.js
@@ -1,27 +1,55 @@
-import React from 'react'
-
+import React, { useEffect  } from 'react'
+import { useCryptoCoins } from "../../../../../config/Coins"
 export default function Spot() {
+
+    const { CryptoCoins, Coins, error, isLoading } = useCryptoCoins()
+
+    let filtCoin = ["btc", "doge","eth", "gala", "link", "ltc", "trx", "xrp", "matic", "sol"]
+
+    useEffect(()=>{
+      CryptoCoins()
+    },[])
+
+
   return (
     <div className='homeCoindrop'>
-        <div className="homeCoindrop-container">
+         { isLoading && <div className="isloading-coin">
+            <h1>Loading...</h1>
+        </div> }
+        { error && <div className="isloading-coin">
+            <h1>Network Error</h1>
+        </div> }
+
+     {Coins &&
+      Coins.filter(newEl => filtCoin.includes(newEl.symbol)).map((coin) => (
+        <div key={coin.id} className="homeCoindrop-container">
             <div className="coin-pair">
                 <div className="coin-pair-container">
                     <div className="imgae">
-                      <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="" />
+                        <img src={coin.image} alt="" />
                     </div>
                     <div className="pair">
-                        <h3>BTC/<span>USDT</span></h3>
+                        <h3>{coin.symbol}/<span>USDT</span></h3>
                     </div>
                 </div>
             </div>
             <div className="coin-container">
                 <div className="coin-content">
-                    <h4>23573.35</h4>
+                    <h4>{coin.current_price}</h4>
                 </div>
             </div>
             <div className="coin-percent">
               <div className="coin-percent-container">
-                  <p>-2.45%</p>
+                  {coin.price_change_percentage_24h >= 0 && (
+               <p className="positive">
+                    +{parseFloat(coin.price_change_percentage_24h).toFixed(2)}%{" "}
+                </p>
+                )}
+                {coin.price_change_percentage_24h < 0 && (
+                    <p className="negative">
+                        {parseFloat(coin.price_change_percentage_24h).toFixed(2)}%
+                    </p>
+                )}
               </div>
             </div>
             <div className="trade-section">
@@ -35,6 +63,7 @@ export default function Spot() {
                 </div>
             </div>
         </div>
+    ))}
     </div>
   )
 }
