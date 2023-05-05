7b55273ba820401cae12e141ff6a53e130352e3c
Highscore <highscoretechng@gmail.com>
2023-05-05 22:18:44 +0100

    home desktop page

--- src/Pages/components/market/views/desktop/routes/perpetualRoute/Coin.js
+++ src/Pages/components/market/views/desktop/routes/perpetualRoute/Coin.js
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
+
+import { useCryptoCoins } from '../../../../../../config/Coins' 
 
 export default function Coin() {
+    const [ starred, setStarred ] = useState(false)
+
+    const handleStarred = (()=>{
+      if(starred){
+        setStarred(false)
+      }else{
+        setStarred(true)
+      }
+    })
+  
+  const { CryptoCoins, Coins, isLoading, error } = useCryptoCoins()
+  let filtCoin = [ "btc","eth", "dot",  "xrp", "doge", ]
+  useEffect(()=>{
+    CryptoCoins()
+  },[])
+  
   return (
-    <div>Coin</div>
+    <div className='desktop-market-coins'>
+
+    { isLoading && <div className="isloading-coin">
+            <h1>Loading...</h1>
+        </div> }
+        { error && <div className="isloading-coin">
+            <h1>Network Error</h1>
+        </div> }
+
+        {Coins &&
+          Coins.filter(newEl => filtCoin.includes(newEl.symbol)).map((coin) => (
+    <div key={coin.id} className="desktop-market-coins-container">
+        <div className="logo-pairs">
+            <div className="star">
+                <h3 onClick={handleStarred}> { starred ? <AiFillStar /> : <AiOutlineStar /> } </h3>
+            </div>
+            <div className="logo">
+                <img src={coin.image} alt="" />
+            </div>
+            <div className="pair">
+                <h4>{coin.symbol}USD</h4>
+            </div>
+        </div>
+        <div className="price">
+            <h4>{coin.current_price}</h4>
+        </div>
+        <div className="change-percent">
+            {coin.price_change_percentage_24h >= 0 && (
+                <h4 className="positive">
+                  +{parseFloat(coin.price_change_percentage_24h).toFixed(2)}%{" "}
+                </h4>
+              )}
+              {coin.price_change_percentage_24h < 0 && (
+                <h4 className="negative">
+                    {parseFloat(coin.price_change_percentage_24h).toFixed(2)}%
+                </h4>
+              )}
+        </div>
+        <div className="high">
+            <h4>{coin.high_24h}</h4> 
+        </div>
+        <div className="high">
+            <h4>{coin.low_24h}</h4>
+        </div>
+        <div className="high">
+            <h4>{coin.total_volume}</h4>
+        </div>
+        <div className="trade">
+            <h4>Trade</h4>
+        </div>
+    </div>
+    ))}
+</div>
   )
 }
--- src/Pages/components/market/views/desktop/routes/perpetualRoute/Usdt.js
+++ src/Pages/components/market/views/desktop/routes/perpetualRoute/Usdt.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
+
+import { useCryptoCoins } from '../../../../../../config/Coins' 
 
 export default function Usdt() {
+
+    const [ starred, setStarred ] = useState(false)
+    let filtCoin = [ "btc","eth", "dot",  "xrp", "doge","trx", "matic", "sol", "link", "1inch", "aave", "algo", "alice", "ada", "epe", "apt", "axs", "bal" ,
+     "avax", "band", "bat", "bch", "bnb" , "dash", "chz", "celo" ]
+    const handleStarred = (()=>{
+      if(starred){
+        setStarred(false)
+      }else{
+        setStarred(true)
+      }
+    })
+  
+  const { CryptoCoins, Coins, isLoading, error } = useCryptoCoins()
+  
+  useEffect(()=>{
+    CryptoCoins()
+  },[])
+  
+
   return (
-    <div>Usdt</div>
+    <div className='desktop-market-coins'>
+    { isLoading && <div className="isloading-coin">
+        <h1>Loading...</h1>
+    </div> }
+    { error && <div className="isloading-coin">
+        <h1>Network Error</h1>
+    </div> }
+
+    {Coins &&
+          Coins.filter(newEl => filtCoin.includes(newEl.symbol)).map((coin) => (
+    <div key={coin.id} className="desktop-market-coins-container">
+        <div className="logo-pairs">
+            <div className="star">
+                <h3 onClick={handleStarred}> { starred ? <AiFillStar /> : <AiOutlineStar /> } </h3>
+            </div>
+            <div className="logo">
+                <img src={coin.image} alt="" />
+            </div>
+            <div className="pair">
+                <h4>{coin.symbol}USDT</h4>
+            </div>
+        </div>
+        <div className="price">
+            <h4>{coin.current_price}</h4>
+        </div>
+        <div className="change-percent">
+            {coin.price_change_percentage_24h >= 0 && (
+                <h4 className="positive">
+                  +{parseFloat(coin.price_change_percentage_24h).toFixed(2)}%{" "}
+                </h4>
+            )}
+            {coin.price_change_percentage_24h < 0 && (
+            <h4 className="negative">
+                {parseFloat(coin.price_change_percentage_24h).toFixed(2)}%
+            </h4>
+            )}
+        </div>
+        <div className="high">
+            <h4>{coin.high_24h}</h4> 
+        </div>
+        <div className="high">
+            <h4>{coin.low_24h}</h4>
+        </div>
+        <div className="high">
+            <h4>{coin.total_volume}</h4>
+        </div>
+        <div className="trade">
+            <h4>Trade</h4>
+        </div>
+    </div>
+    ))}
+</div>
   )
 }
