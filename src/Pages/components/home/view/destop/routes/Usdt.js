478c86bf929b167a36fb4921ed58bf437395d6f4
Highscore <highscoretechng@gmail.com>
2023-05-05 17:33:57 +0100

    market route

--- src/Pages/components/home/view/destop/routes/Usdt.js
+++ src/Pages/components/home/view/destop/routes/Usdt.js
@@ -1,40 +1,69 @@
-import React from 'react'
+import React, { useEffect  } from 'react'
+import { useCryptoCoins } from "../../../../../config/Coins"
 
 export default function Usdt() {
+
+    const { CryptoCoins, Coins, error, isLoading } = useCryptoCoins()
+
+    let filtCoin = ["btc", "dot","fil", "gala", "link", "ltc", "trx", ]
+
+    useEffect(()=>{
+      CryptoCoins()
+    },[])
+
   return (
     <div className='homeCoindrop'>
-      <div className="homeCoindrop-container">
-          <div className="coin-pair">
-              <div className="coin-pair-container">
-                  <div className="imgae">
-                    <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="" />
-                  </div>
-                  <div className="pair">
-                      <h3>BTC/<span>USD</span></h3>
-                  </div>
-              </div>
-          </div>
-          <div className="coin-container">
-              <div className="coin-content">
-                  <h4>23573.35</h4>
-              </div>
-          </div>
-          <div className="coin-percent">
-            <div className="coin-percent-container">
-                <p>-2.45%</p>
-            </div>
-          </div>
-          <div className="trade-section">
-              <div className="trade-btn-container">
-                  <div className="trade-btn-content">
-                      <button>Buy</button>
-                  </div>
-                  <div className="trade-btn-content">
-                      <button>Trade</button>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
+    { isLoading && <div className="isloading-coin">
+       <h1>Loading...</h1>
+   </div> }
+   { error && <div className="isloading-coin">
+       <h1>Network Error</h1>
+   </div> }
+
+{Coins &&
+ Coins.filter(newEl => filtCoin.includes(newEl.symbol)).map((coin) => (
+   <div key={coin.id} className="homeCoindrop-container">
+       <div className="coin-pair">
+           <div className="coin-pair-container">
+               <div className="imgae">
+                   <img src={coin.image} alt="" />
+               </div>
+               <div className="pair">
+                   <h3>{coin.symbol}USDT</h3>
+               </div>
+           </div>
+       </div>
+       <div className="coin-container">
+           <div className="coin-content">
+               <h4>{coin.current_price}</h4>
+           </div>
+       </div>
+       <div className="coin-percent">
+         <div className="coin-percent-container">
+             {coin.price_change_percentage_24h >= 0 && (
+          <p className="positive">
+               +{parseFloat(coin.price_change_percentage_24h).toFixed(2)}%{" "}
+           </p>
+           )}
+           {coin.price_change_percentage_24h < 0 && (
+               <p className="negative">
+                   {parseFloat(coin.price_change_percentage_24h).toFixed(2)}%
+               </p>
+           )}
+         </div>
+       </div>
+       <div className="trade-section">
+           <div className="trade-btn-container">
+               <div className="trade-btn-content">
+                   <button>Buy</button>
+               </div>
+               <div className="trade-btn-content">
+                   <button>Trade</button>
+               </div>
+           </div>
+       </div>
+   </div>
+))}
+</div>
   )
 }
