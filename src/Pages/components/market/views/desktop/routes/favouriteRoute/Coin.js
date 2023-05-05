d09bf655b7cf7e16574567661772aad2568412e0
Highscore <highscoretechng@gmail.com>
2023-05-05 22:52:49 +0100

    home desktop page

--- src/Pages/components/market/views/desktop/routes/favouriteRoute/Coin.js
+++ src/Pages/components/market/views/desktop/routes/favouriteRoute/Coin.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { MdOutlineContentPasteSearch} from "react-icons/md";
 
 export default function Coin() {
   return (
-    <div>Coin</div>
+    <div className="no-data">
+        <div className="no-data-container">
+            <div className="no-data-content">
+                <h4>No data</h4>
+                <h3><MdOutlineContentPasteSearch /></h3>
+            </div>
+        </div>
+    </div>
   )
 }
--- src/Pages/components/market/views/desktop/routes/favouriteRoute/Usdt.js
+++ src/Pages/components/market/views/desktop/routes/favouriteRoute/Usdt.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import { MdOutlineContentPasteSearch} from "react-icons/md";
 export default function Usdt() {
   return (
-    <div>Usdt</div>
+    <div className="no-data">
+        <div className="no-data-container">
+            <div className="no-data-content">
+                <h4>No data</h4>
+                <h3><MdOutlineContentPasteSearch /></h3>
+            </div>
+        </div>
+    </div>
   )
 }
