16c791dc1570c62cd63e082783bf0618704146ae
Highscore <highscoretechng@gmail.com>
2023-05-07 11:11:59 +0100

    home desktop page

--- src/Pages/components/spot/views/desktop/Classic.js
+++ src/Pages/components/spot/views/desktop/Classic.js
@@ -2,11 +2,24 @@ import React from 'react'
 
 export default function Classic() {
   return (
-    <div>
-      <h3>1</h3>
-      <h3>2</h3>
-      <h3>4</h3>
-      <h3>3</h3>
+    <div className='spot-classic'>
+        <div className="spot-classic-container">
+            <div className="spot-classic-route">
+                <div className="spot-classic-route-conatiner">
+                    <h3>Route</h3>
+                </div>
+            </div>
+            <div className="spot-classic-chart">
+              <div className="spot-classic-chart-container">
+                  <h3>Candle chart</h3>
+              </div>
+            </div>
+            <div className="spot-classic-orderbook">
+              <div className="spot-classic-orderbook-container">
+                  <h3>Order Book</h3>
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
